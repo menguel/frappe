179f1901871c9c40a471bb98b16b267aa409ade0
tnougosso <tnougosso@irex.aretex.ca>
2022-04-06 10:08:19 -0400

    remove console.log in login.js file

--- frappe/templates/includes/login/login.js
+++ frappe/templates/includes/login/login.js
@@ -51,9 +51,6 @@ var css = document.createElement("link");
 css.rel = 'stylesheet';
 css.type = "text/css";
 css.href = "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css";
-css.onload = function () {
-	console.log("IREX");
-};
 document.head.appendChild(css);
 
 let phoneInput;
